a51c1c8f3e2aa1602317c36630fdf7d5267f3a9f
zwsutherland <zwsutherland@gmail.com>
2025-02-15 12:51:25 -0500

    week6 code

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+
 public class Node
 {
     public int Data { get; set; }
@@ -12,12 +14,16 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data) {return;}
+
 
         if (value < Data)
         {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
+            else if (Left.Data == value)
+                return;
             else
                 Left.Insert(value);
         }
@@ -26,6 +32,8 @@ public class Node
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
+            else if (Right.Data == value)
+                return;
             else
                 Right.Insert(value);
         }
@@ -33,13 +41,53 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data) {return true;}
+        else if (value < Data)
+        {
+            if (Left == null)
+            {
+                return false;
+            }
+            else
+            {
+                return Left.Contains(value);
+            }
+        }
+        else
+        {
+            if (Right == null)
+            {
+                return false;
+            }
+            else
+            {
+                return Right.Contains(value);
+            }
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int l = 0;
+        int r = 0;
+
+        if (Left != null)
+        {
+            l = Left.GetHeight();
+        }
+
+        if (Right != null)
+        {
+            r = Right.GetHeight();
+        }
+
+        if (l >= r)
+        {
+            return l+1;
+        }
+        else
+        {
+            return r+1;
+        }
     }
 }
--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -48,6 +48,18 @@ public static class Trees
     /// <param name="bst">the BinarySearchTree in which to insert the values</param>
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
     {
-        // TODO Start Problem 5
+        if (first > last)
+        {
+            return;
+        }
+
+        int middle = (first + last) / 2;
+
+        bst.Insert(sortedNumbers[middle]);
+
+        InsertMiddle(sortedNumbers, first, middle - 1, bst);
+        InsertMiddle(sortedNumbers, middle + 1, last, bst);
+
+        return;
     }
 }
